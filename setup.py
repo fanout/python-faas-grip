a1be999a0ea095bef5cb1ce1e941141928df2a0a
Justin Karneges <justin@karneges.com>
2017-12-03 16:28:57 -0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 name='faas-grip',
-version='0.1.0',
+version='1.0.0',
 description='FaaS GRIP library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
